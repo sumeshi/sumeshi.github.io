4211ede167f8fe3df73de268b3a6b4d0307895f2
sumeshi <sumeshi@users.noreply.github.com>
2020-06-15 23:03:22 +0000

    deploy: 8eb1c55dc9ff9ea8b0d8e05d9d0cefec7c343313

--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5a284e2baba10a84881af3741bbd17fe.js"
+  "/precache-manifest.82c2690915fa472f0786172a948ab5cc.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "sumeshi.github.io"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,5 +31,4 @@ workbox.core.setCacheNameDetails({prefix: "sumeshi.github.io"});
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
