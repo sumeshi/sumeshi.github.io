9405646fe8eafd32184cbdb6f953e70b2e9b06e9
sumeshi <j15322sn@gmail.com>
2021-08-26 03:14:56 +0900

    feat: add sanitize

--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -17,9 +17,11 @@
               </v-card-text>
             </v-col>
           </v-row>
+          <v-divider style="margin-bottom: 2em;" />
           <v-layout class="wrap" style="max-width: 100%; height: auto;" >
-            <v-card-text class="html-wrapper" v-html="post.html_text" />
+            <v-card-text class="html-wrapper" v-html="$sanitize(post.html_text)" />
           </v-layout>
+          <v-divider style="margin: 2em 0 2em 0;" />
           <v-card-actions class="justify-center">
             <v-btn class="btn" @click="$router.go(-1)">
               <span class="caption">back</span>
@@ -36,6 +38,7 @@ import Vue from 'vue'
 import { Component } from 'nuxt-property-decorator'
 
 import axios from 'axios'
+import sanitizeHTML from 'sanitize-html'
 
 interface PostContent {
   title: string
@@ -43,6 +46,10 @@ interface PostContent {
   html_text: string
   published_at: string
 }
+
+sanitizeHTML.defaults.allowedTags.push('img')
+sanitizeHTML.defaults.allowedAttributes.img.push('style')
+Vue.prototype.$sanitize = sanitizeHTML
 
 @Component({
 })
@@ -58,7 +65,7 @@ export default class Post extends Vue {
     axios.get(`https://sumeshi.github.io/api/posts/${this.$route.params.id}/index.html`).then(
       (res) => {
         this.post = res.data
-        this.post.html_text = this.post.html_text.replaceAll('<img', '<img style="max-width: 100%; margin: 2em 0 2em 0;"')
+        this.post.html_text = this.post.html_text.replaceAll('<img', '<img style="width: 100%; margin: 1em 0 1em 0"')
       }
     )
   }
