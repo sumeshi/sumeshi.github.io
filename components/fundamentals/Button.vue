90e1ecb7e0bf02c828a264ee8bbc6490fb1f11fd
sumeshi <sum3sh1@protonmail.com>
2022-04-02 18:21:51 +0900

    feat: add fundamental components

---
+++ components/fundamentals/Button.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-btn class="btn" :to="link" :color="color" raised>{{message}}</v-btn>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  props: {
+    message: {
+      type: String,
+      required: true
+    },
+    link: {
+      type: String,
+      required: true
+    },
+    color: {
+      type: String,
+      required: false,
+      default: ""
+    }
+  },
+
+  setup() {
+  },
+})
+</script>
+
+
+<style lang="scss">
+.btn {
+  width: 120px;
+}
+</style>
