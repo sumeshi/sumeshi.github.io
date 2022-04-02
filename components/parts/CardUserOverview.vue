03eeb675ca0e1232fd67135e6e9df158d607cfa7
sumeshi <sum3sh1@protonmail.com>
2022-04-02 18:22:18 +0900

    feat: generalize components

---
+++ components/parts/CardUserOverview.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-card class="pt-2 pb-2 pr-4 pl-4" justify="center">
+    <v-row>
+      <v-col>
+        <v-card class="elevation-0">
+          <v-card-text>
+          <h3>{{name}}</h3>
+          <p class="position mt-2 overline grey--text text--darken-2">
+            {{position}}
+          </p>
+          <div v-for="link of links" :key="link">
+            <p>{{link.name}}: <a :href="link.url">{{link.message}}</a></p>
+          </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col class="d-none d-sm-flex justify-center align-center">
+        <v-card class="elevation-0">
+          <v-card-actions>
+            <Icon :src="icon.src" :link="icon.link" />
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-card class="elevation-0">
+      <v-card-actions class="d-flex d-sm-none justify-center">
+        <Button :message="button.message" :link="button.link" />
+      </v-card-actions>
+    </v-card>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    position: {
+      type: String,
+      required: true
+    },
+    icon: {
+      type: Object,
+      required: true,
+    },
+    button: {
+      type: Object,
+      required: true,
+    },
+    links: {
+      type: Array,
+      required: false,
+      default: []
+    }
+  },
+
+  setup() {
+  },
+})
+</script>
+
+<style lang="scss">
+.position {
+  line-height: 1rem !important;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,13 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="12" md="8">
-      <v-card class="logo pa-4" justify="center">
-        <v-row>
-          <v-card class="elevation-0 col-6">
-            <v-card-text>
-              <h3>S.Nakano</h3>
-              <p class="overline grey--text text--darken-2">
-                <span>DFIR Researcher / </span><span>Software Developer</span>
-              </p>
-              <p>twitter: <a href="https://twitter.com/sum3sh1">@sum3sh1</a></p>
-            </v-card-text>
-          </v-card>
-          <v-card class="elevation-0 col-6">
-            <v-card-text class="d-none d-sm-flex justify-center">
-              <router-link to="/about">
-                <v-hover v-slot="{hover}">
-                  <v-avatar size="120" :elevation="hover ? 12 : 2" :class="{'on-hover': hover}">
-                    <img src="glitchme.jpg" alt="me">
-                  </v-avatar>
-                </v-hover>
-              </router-link>
-            </v-card-text>
-          </v-card>
-        </v-row>
-        <v-card class="elevation-0">
-          <v-actions class="d-flex d-sm-none justify-center">
-            <v-btn to="/about" raised class="btn">see more</v-btn>
-          </v-actions>
-        </v-card>
-      </v-card>
+    <v-col cols="12" sm="8">
+      <CardUserOverview 
+        name="S.Nakano"
+        position="DFIR Researcher / Software Developer"
+        :icon="{src: 'glitchme.jpg', link: '/about'}"
+        :button="{message: 'see more', link: '/about'}"
+        :links="[{name: 'twitter', url: 'https://twitter.com/sum3sh1', message: '@sum3sh1'}]"
+      />
       <v-card class="logo mt-5 pa-4 justify-center">
         <v-card-text>
           <h2>Latest Posts</h2>
@@ -92,21 +70,4 @@ export default class Home extends Vue {
 </script>
 
 <style lang="scss" scoped>
-
-span {
-  display: inline-block;
-}
-
-.btn {
-  width: 120px;
-}
-.v-avatar {
-  opacity: 0.6;
-  transition: opacity .2s ease-in-out;
-}
-
-.v-avatar:not(.on-hover) {
-  opacity: 1.0;
- }
-
 </style>
