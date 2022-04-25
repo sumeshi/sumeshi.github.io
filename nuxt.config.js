dfe42de5892595e1af8a7a5b9e5917ca0a36467b
sumeshi <sum3sh1@protonmail.com>
2022-04-26 01:32:41 +0900

    feat: add verification code

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,8 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      {name: 'google-site-verification', content: 'oKJDJNVxCY8_al6JndvVYqrjdywDE_5S6ypSCNNFKc8' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
