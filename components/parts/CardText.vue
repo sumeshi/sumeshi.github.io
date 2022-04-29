d44761bc4b906214728ca9350e6b3d17e8e31eb5
sumeshi <sum3sh1@protonmail.com>
2022-04-30 03:33:49 +0900

    feat: add cardtext

---
+++ components/parts/CardText.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card class="mt-5 pa-4" justify="center">
+        <v-card class="elevation-0">
+          <v-card-text>
+            <h3 class="mb-4">{{title}}</h3>
+            <div class="position" v-for="line of text" :key="line.id">
+              <p class="lines">{{ line }}</p>
+            </div>
+          </v-card-text>
+        </v-card>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      required: false
+    },
+    text: {
+      type: Array,
+      required: false
+    },
+  },
+  setup() {
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.lines {
+  margin: 0;
+  line-height: 1.5rem !important
+}
+</style>
