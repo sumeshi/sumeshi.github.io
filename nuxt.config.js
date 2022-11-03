4d20a188c53cdbca38b170da27716f1033d34faf
sumeshi <sum3sh1@protonmail.com>
2022-11-03 23:58:29 +0900

    feat: add metadata

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,9 +15,20 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
-      {name: 'google-site-verification', content: 'oKJDJNVxCY8_al6JndvVYqrjdywDE_5S6ypSCNNFKc8' }
+      { hid: 'description', name: 'description', content: 'DFIR Blog' },
+      { hid: 'keywords', name: 'keywords', content: 'Security,DigitalForensics,Python' },
+
+      { hid: 'og:site_name', property: 'og:site_name', content: 'sumeshi.github.io' },
+      { hid: 'og:type', property: 'og:type', content: 'blog' },
+      { hid: 'og:url', property: 'og:url', content: 'https://sumeshi.github.io' },
+      { hid: 'og:title', property: 'og:title', content: 'sumeshi.github.io' },
+      { hid: 'og:description', property: 'og:description', content: 'DFIR Blog' },
+      // { hid: 'og:image', property: 'og:image', content: '' },
+
+      // { hid: 'twitter:card', name: 'twitter:card', content: '' },
+      { hid: 'twitter:site', name: 'twitter:site', content: '@sum3sh1' }
+      { name: 'google-site-verification', content: 'oKJDJNVxCY8_al6JndvVYqrjdywDE_5S6ypSCNNFKc8' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
