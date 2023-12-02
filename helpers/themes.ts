d89b1f6473ced91da8fad87f09be8c2ce4d40aec
S.Nakano <35072092+sumeshi@users.noreply.github.com>
2023-12-02 09:32:45 +0000

    fix: update dependencies

---
+++ helpers/themes.ts
@@ -0,0 +1,16 @@
+import type { ThemeDefinition } from 'vuetify'
+
+export const MAIN_DARK_THEME = 'mainDarkTheme'
+export const mainDarkTheme: ThemeDefinition = {
+  dark: true,
+  colors: {
+    background: '#0C111B',
+    // surface: '#1f2937',
+    // primary: '#6366f1',
+    // secondary: '#9333ea',
+    // error: '#ef4444',
+    // info: '#3b82f6',
+    // success: '#22c55e',
+    // warning: '#f59e0b',
+  },
+}
--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -2,13 +2,22 @@
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { MAIN_DARK_THEME, mainDarkTheme } from '@/helpers/themes'
 
 export default defineNuxtPlugin(nuxtApp => {
   const vuetify = createVuetify({
     components,
     directives,
     theme: {
-      defaultTheme: 'dark',
+      defaultTheme: MAIN_DARK_THEME,
+      themes: {
+        mainDarkTheme,
+      },
+      variations: {
+        colors: ['primary', 'secondary', 'accent'],
+        lighten: 9,
+        darken: 9,
+      },
     },
   })
 
