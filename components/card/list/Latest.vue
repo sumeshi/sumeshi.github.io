34c14def15269cd08ebfd54dd2432c2b742ff696
S.Nakano <35072092+sumeshi@users.noreply.github.com>
2024-05-10 23:25:21 +0000

    feat: category button

--- components/card/list/Latest.vue
+++ components/card/list/Latest.vue
@@ -12,8 +12,14 @@
           exact
         >
           <v-list-item-action>
-            <div class="pb-2">
-              <span class="text-caption">{{ postIndex.published_at.replace('T', ' ') }}</span>
+            <div class="pb-2 w-100">
+              <div class="d-flex">
+                <span class="text-caption">{{ postIndex.published_at.replace('T', ' ') }}</span>
+                <v-spacer></v-spacer>
+                <v-chip-group>
+                  <v-chip class="categorybutton justify-center" variant="tonal" size="x-small" :to="'/posts/' + postIndex.path.replace('/posts/', '').replace(/\/.*/, '')"><span class="text-uppercase" style="font-size: 10px;">{{ postIndex.path.replace('/posts/', '').replace(/\/.*/, '') }}</span></v-chip>
+                </v-chip-group>
+              </div>
               <h3>{{postIndex.heading ? postIndex.heading : postIndex.title}}</h3>
               <p class="text-body-2">{{postIndex.description}}</p>
             </div>
@@ -36,4 +42,7 @@ const props = withDefaults(defineProps<Props>(), {
 </script>
 
 <style lang="scss" scoped>
+.categorybutton {
+  width: 120px;
+}
 </style>>
