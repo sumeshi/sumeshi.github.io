a440ca077568b88c7b163621df29526b86539be6
S.Nakano <35072092+sumeshi@users.noreply.github.com>
2024-12-10 16:24:00 +0000

    fix: dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,21 +32,26 @@ export default defineNuxtConfig({
       ]
     }
   },
+
   css: [
     '@/assets/styles/main.scss',
     'vuetify/lib/styles/main.sass',
     '@mdi/font/css/materialdesignicons.min.css',
   ],
+
   modules: ['nuxt-gtag'],
+
   gtag: {
     id: 'G-DVLS4LMPGV',
     config: {
       page_title: 'sumeshi.github.io'
     },
   },
+
   build: {
     transpile: ['vuetify'],
   },
+
   vite: {
     define: {
       'process.env.DEBUG': false,
@@ -58,5 +63,7 @@ export default defineNuxtConfig({
         }
       }
     }
-  }
+  },
+
+  compatibilityDate: "2024-12-06"
 })
