6bf2680112a53aad61e07bab322e0c6586ea1af4
sumeshi <sum3sh1@protonmail.com>
2026-08-11 14:59:54 +0900

    fix: dependencies

--- .devcontainer/postCreateCommands.fish
+++ .devcontainer/postCreateCommands.fish
@@ -2,7 +2,7 @@
 
 # enable pinned pnpm from package.json
 corepack enable
-corepack prepare pnpm@11.9.0 --activate
+corepack prepare pnpm@11.21.0 --activate
 
 # install project dependencies
 pnpm install --frozen-lockfile
